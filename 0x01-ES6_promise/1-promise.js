ff25e24148a52771c3bdab2a503d2b23153b0829
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-05 18:18:14 +0000

    promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((good, error) => {
     if (success) {
-      good({status: 200, body: 'Succes',});
+      good({status: 200, body: 'Success',});
     } else {
-      error('The fake API is not working currently')
+      error(Error('The fake API is not working currently'));
     }
-  )};
+  });
 }
