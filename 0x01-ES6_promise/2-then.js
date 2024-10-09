f485eafb5856febe6427ac3c30197b8de19f3f64
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-09 02:30:42 +0000

    handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,5 +2,5 @@ export default function handleResponseFromAPI(promise) {
   return Promise
   .then(() => {{ status: 200, body: 'success' }})
   .catch(() => Error())
-  .finally('Got a response from the API')
+  .finally(() = > 'Got a response from the API'));
 }
