83e9d7a50b2524e47d1a4a4cf413139f2a673a8f
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-09 21:48:32 +0000

    try, catch, finally

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+function guardrail(mathFunction) {
+  let queue = [];
+  const i = mathFunction(/;
+  try {
+    queue.push(i);
+  } catch {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
