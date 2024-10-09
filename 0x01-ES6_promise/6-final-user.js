e1fd1cc18d00f67a3869fa0c2ba0d19d4f09ffdd
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-09 17:35:53 +0000

    handling promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
   return Promise.allSettled([user, photo])
-    .then((res) => res.map((i) => ({ status: i.status, value: (i.status === 'rejected') ? i.reason : i.value })));
+    .then((res) => res.map((i) => ({ status: i.status, value: (i.status === 'rejected') ? String(i.reason) : i.value })));
 }
