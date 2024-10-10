bac86e1de09479a1f41b0ea2a5c3d94da6ea8b0d
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-10 21:07:42 +0000

    cls, getter and seter

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  set name(value) {
+    if (typeof value !== 'string')
+  	  throw new TypeError('name must be a string');
+    this._name = value;
+  }
+  get length() {
+  	return this._length;
+  }
+
+  set length(value) {
+  	if (typeof value !== 'number') {
+  		throw new TypeError('Length must be a number');
+  	}
+  	this._length = value;
+
+  }
+
+  get students() {
+  	return this._students;
+  }
+
+  set students(value) {
+  	if (!Array.isArray(value) || value.every((elem) => typeof elem !== 'string')) {
+  		throw new TypeError('must be a array');
+  	}
+  	this._students = value;
+  }
+}
