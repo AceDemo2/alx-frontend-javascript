d00b8b1a0ab2cb28ebe82ebd6a33b7d0cb3ab9c7
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-11 02:26:35 +0000

    building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,16 @@
 export default class Building {
   constructor(sqft) {
-    // Prevent direct instantiation of Building
-    if (this.constructor === Building) {
-      throw new Error('Cannot instantiate an abstract class');
-    }
-
-    // Ensure that any class extending Building implements evacuationWarningMessage
-    if (typeof this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building && typeof this.evacuationWarningMesagge !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 }
