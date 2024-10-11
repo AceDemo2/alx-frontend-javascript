9627a2dcbc88c8f9347873e4afa11b3dbb6aead6
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-11 01:37:47 +0000

    building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+// 5-building.js
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new Error('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+
+  // Abstract method enforcement
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
