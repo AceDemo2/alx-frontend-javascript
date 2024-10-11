be6551bed36ddf067bc9ce464c8b783335607fd6
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-11 02:47:54 +0000

    extend

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,4 +1,4 @@
-import Building from './5-building'
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
@@ -18,6 +18,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return 'Evacuate slowly the ${this.floors} floors';
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
