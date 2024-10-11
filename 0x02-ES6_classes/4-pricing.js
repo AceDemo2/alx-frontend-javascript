47b354b82fe2759046d70f7bb5339d04b4e0544b
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-11 00:57:22 +0000

    currency[D

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    if (typeof val !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    if (!(val instanceof Currency)) {
+      throw new Error('Currency must be a currency');
+    }
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new Error('Both amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
