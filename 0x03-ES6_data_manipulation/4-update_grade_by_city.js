0c6fba20fe821e9a055ed77b9841f3df1d5557e5
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-16 01:33:14 +0000

    grades

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,22 +1,16 @@
 export default function updateStudentGradeByCity(lists, city, newGrades) {
   const filteredList = lists.filter((i) => i.location === city);
-
   const newList = filteredList.map((j) => {
-    const j1 = { ...j }; // Create a shallow copy of the student object
-
+    const j1 = { ...j };
     for (const k of newGrades) {
       if (j1.id === k.studentId) {
-        j1.grade = k.grade; // Update the grade in the copied object
+        j1.grade = k.grade;
       }
     }
-
-    // If the grade is undefined, set it to 'N/A'
     if (j1.grade === undefined) {
       j1.grade = 'N/A';
     }
-
-    return j1; // Return the new object without modifying the original one
+    return j1;
   });
-
   return newList;
 }
