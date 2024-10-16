8fbd29b8cdbcc7e3763ea437c2e38f7e590c5943
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-16 15:08:33 +0000

    has

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,2 +1,11 @@
 export default function hasValuesFromArray(myset, arr) {
-  
+  let j = 1;
+  for (let i of arr) {
+    if (myset.has(i)) {
+      continue;
+    }
+    j = 0;
+    break;
+  }
+  return j ? true : false
+}
