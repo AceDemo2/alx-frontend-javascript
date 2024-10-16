a6dba753664c39636c3611d2bcca11a36a3af26e
AceDemo2 <lawaladepoju705@gmail.com>
2024-10-16 01:21:16 +0000

    grades

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,16 @@
 export default function updateStudentGradeByCity(lists, city, newGrades) {
-  const filtered_list = lists.filter((i) => i.location === city);
-  const new_list = filtered_list.map(function (j) {
-    for (let k of newGrades) {
-      if (j.id === k.studentId) {
-        j['grade'] = k.grade;
+  const filteredList = lists.filter((i) => i.location === city);
+  const newList = filteredList.map((j) => {
+    const j1 = j;
+    for (const k of newGrades) {
+      if (j1.id === k.studentId) {
+        j1.grade = k.grade;
       }
     }
-    if (j['grade'] === 'undefined') {
-        j['grade'] = 'N/A';
-      }
-    return j
+    if (j1.grade === 'undefined') {
+      j1.grade = 'N/A';
+    }
+    return j1;
   });
-  return new_list;
+  return newList;
 }
